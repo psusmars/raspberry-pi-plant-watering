48b98f5cecb7bf65933a81f6aaaa316402ab02ca
Paul Susmarski <paul.susmarski@gmail.com>
2019-07-15 15:49:56 -0600

    only water for 2 seconds as we are a small plant

--- water.py
+++ water.py
@@ -33,7 +33,7 @@ def auto_water(delay = 5, pump_pin = 7, water_sensor_pin = 8):
             time.sleep(delay)
             wet = get_status(pin = water_sensor_pin) == dry
             if not wet:
-                if consecutive_water_count < 5:
+                if consecutive_water_count < 1:
                     pump_on(pump_pin, 1)
                 consecutive_water_count += 1
             else:
