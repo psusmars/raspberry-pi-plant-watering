740c4f847bda561f783f8bb1a1e7b3126303b537
Paul Susmarski <paul.susmarski@gmail.com>
2020-08-23 17:53:48 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /var/flaskapp/water
+sudo mkdir -p /var/flaskapp/water
 cp -r * /var/flaskapp/water
 sudo chown -R www-data:www-data /var/flaskapp/water
 sudo cp water.conf /etc/supervisor/conf.d/water.conf
